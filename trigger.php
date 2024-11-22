4e2d1760da8cbc7da957bc5e13348f57e21700d8
Abdul Rauf <sigidevelopers@gmail.com>
2024-11-22 12:42:13 +0500

    php

--- trigger.php
+++ trigger.php
@@ -6,14 +6,17 @@ $workingDir = '/home/trimworldwide/rauf.trimworldwide.com';
 $nodePath = '/home/trimworldwide/.nvm/versions/node/v18.20.4/bin/node';
 $npmPath = '/home/trimworldwide/.nvm/versions/node/v18.20.4/bin/npm';
 
+// Export PATH for the script to find node and npm
+$exportPath = 'export PATH=$PATH:/home/trimworldwide/.nvm/versions/node/v18.20.4/bin';
+
 // Set permissions for node and npm binaries
 $setBinaryPermissions = shell_exec("chmod +x $nodePath $npmPath 2>&1");
 
 // Set permissions for the working directory
-$setDirPermissions = shell_exec("chmod -R 777 $workingDir 2>&1");
+$setDirPermissions = shell_exec("chmod -R 775 $workingDir 2>&1");
 
-// Run npm install in the working directory
-$output = shell_exec("cd $workingDir && $npmPath install 2>&1");
+// Run npm install with explicit PATH export
+$output = shell_exec("$exportPath && cd $workingDir && $npmPath install 2>&1");
 
 // Display results
 echo "Binary Permissions Output:<br />" . nl2br($setBinaryPermissions) . "<br />";
