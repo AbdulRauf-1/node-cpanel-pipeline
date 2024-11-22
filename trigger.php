d181a1544077c62214cf82dd5e8a0131c3e2e2cc
Abdul Rauf <sigidevelopers@gmail.com>
2024-11-22 12:20:15 +0500

    php

--- trigger.php
+++ trigger.php
@@ -2,12 +2,16 @@
 // Path to your working directory
 $workingDir = '/home/trimworldwide/rauf.trimworldwide.com';
 
-// Full path to npm
+// Full path to npm (from your terminal output)
 $npmPath = '/home/trimworldwide/.nvm/versions/node/v18.20.4/bin/npm';
 
-// Command to run npm install
+// Ensure permissions are correct before running the command
+$permissionsCheck = shell_exec("chmod -R 755 $workingDir && chmod +x $npmPath 2>&1");
+
+// Run npm install
 $output = shell_exec("cd $workingDir && $npmPath install 2>&1");
 
-// Output the result
-echo nl2br($output);
+// Display permission checks and npm output
+echo "Permissions Check Output:<br />" . nl2br($permissionsCheck) . "<br />";
+echo "NPM Install Output:<br />" . nl2br($output) . "<br />";
 ?>
