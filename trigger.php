7c881fb7095c2012d957ad99be5216d76ecad32c
Abdul Rauf <sigidevelopers@gmail.com>
2024-11-22 12:16:46 +0500

    php

--- trigger.php
+++ trigger.php
@@ -2,8 +2,11 @@
 // Path to your working directory
 $workingDir = '/home/trimworldwide/rauf.trimworldwide.com';
 
+// Full path to npm
+$npmPath = '/home/trimworldwide/.nvm/versions/node/v18.20.4/bin/npm';
+
 // Command to run npm install
-$output = shell_exec("cd $workingDir && npm install 2>&1");
+$output = shell_exec("cd $workingDir && $npmPath install 2>&1");
 
 // Output the result
 echo nl2br($output);
